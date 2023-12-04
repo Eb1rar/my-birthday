530d0b443f82c473b6545a20a66596af6744f585
ebrar <ebrkdmr1@gmail.com>
2023-12-04 12:42:27 +0300

    css

---
+++ src/List.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import data from "./data";
+
+const List = ({ data }) => {
+  return (
+    <div className="card">
+      {data.map(({ id, name, age, image }) => (
+        <div key={id} className="person">
+          <img src={image} alt="{name}" />
+          <div>
+            <h4>{name}</h4>
+            <p>{age}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default List;
